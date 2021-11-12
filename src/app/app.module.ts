af1ce76d27ebfec135ff8671a6295e20a18c6bc5
GuilhermeKopp <guikopp19@gmail.com>
2021-11-12 11:48:24 -0300

    Component create

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -12,13 +14,22 @@ import { ReadAllComponent } from './components/read-all/read-all.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatBadgeModule} from '@angular/material/badge';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { FinalizadosComponent } from './components/finalizados/finalizados.component';
+import { CreateComponent } from './components/create/create.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ReadAllComponent
+    ReadAllComponent,
+    FinalizadosComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +38,14 @@ import {MatIconModule} from '@angular/material/icon';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    HttpClientModule,
+    MatBadgeModule,
+    MatSnackBarModule,
+    FormsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/read-all/read-all.component.ts
+++ src/app/components/read-all/read-all.component.ts
@@ -1,5 +1,7 @@
+import { TodoService } from './../../services/todo.service';
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/models/todo';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-read-all',
@@ -8,21 +10,49 @@ import { Todo } from 'src/app/models/todo';
 })
 export class ReadAllComponent implements OnInit {
 
-  list: Todo[] = [
-    {
-      titulo: "Teste",
-      dataParaFinalizar: new Date,
-      finalizado: false
-    },
-    {
-      titulo: "Teste 2",
-      dataParaFinalizar: new Date,
-      finalizado: false
-    }
-  ]
-  constructor() { }
+  closed = 0;
+
+  list: Todo[] = [];
+  listFinished: Todo[] = [];
+
+  constructor(private service: TodoService, private router: Router) { }
 
   ngOnInit(): void {
+    this.findAll();
   }
 
+  findAll(): void {
+    this.service.findAll().subscribe((resposta) => {
+      resposta.forEach(todo =>{
+        if (todo.finalizado){
+          this.listFinished.push(todo);
+        }else{
+          this.list.push(todo);
+        }
+      })
+      this.closed = this.listFinished.length
+    })
+  }
+
+  finalizar(item: Todo):void {
+    item.finalizado = true;
+    this.service.update(item).subscribe(() => {
+      this.service.message('Task finalizada com sucesso!');
+        this.list = this.list.filter(todo => todo.id !== item.id);
+        this.closed++;
+    })
+  }
+
+  delete(id: any):void {
+    this.service.delete(id).subscribe((resposta) => {
+      if(resposta === null){
+        this.service.message('Task deletada com sucesso!');
+        this.list = this.list.filter(todo => todo.id !== id);
+      }
+    })
+  }
+
+  navegarParaFinalizados():void {
+    this.router.navigate(['finalizados'])
+  }
 }
